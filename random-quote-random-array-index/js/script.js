0c2fe875ddb52383e5b1574e8d47ff6e50e0eddf
Visible <sites@visible.ie>
2020-06-19 18:37:55 +0100

    First working version

--- random-quote-random-array-index/js/script.js
+++ random-quote-random-array-index/js/script.js
@@ -24,8 +24,8 @@ const twelveSidedDie = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
   // 2a) Create a variable that uses `Math.ceil`, `Math.random` and the length of the array parameter to generate a random number
   
   function diceRoll(array) {
-    var randNum = Math.ceil(Math.random() * twelveSidedDie.length);
-    return array;
+    var randNum = Math.ceil(Math.random() * array);
+    return randNum;
   }
 
 
@@ -44,8 +44,8 @@ playBtn.addEventListener('click', () => {
   // 4) Create two variables to store player one and player two scores
     // 4a) Set both variables equal to a call to the `diceRoll` function - Arguments: twelveSidedDie
 
-var player1 = diceRoll(twelveSidedDie[randNum]);
-var player2 = diceRoll(twelveSidedDie[randNum2]);
+var player1 = diceRoll(twelveSidedDie[11]);
+var player2 = diceRoll(twelveSidedDie[11]);
   // 5) Log out the two variables above to test their values — check them out in the console
 
 console.log(player1);
