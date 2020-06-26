53f0bcf957d5b478c84a13b2adb8dbb92ca7c805
Visible <sites@visible.ie>
2020-06-26 14:20:02 +0100

    Exceeds Project Expectations
    chanmges automatically and changes colour on new quote.

--- js/script.js
+++ js/script.js
@@ -10,13 +10,14 @@ let quotes = [
     quote: "The best and most beautiful things in the world cannot be seen or even touched - they must be felt with the heart.",
     source: 'Helen Keller',
     citation: 'Hubspot',
-    tag: 'wisdom'
+    country: 'America'
   },
   {
     quote: "It is during our darkest moments that we must focus to see the light.",
     source: 'Aristotle',
     year: '112 BC',
-    citation: 'Aristotle\'s Weally Wize Words'
+    citation: 'Aristotle\'s Weally Wize Words',
+    country: 'Greece'
   },
   {
     quote: "In the end, it's not the years in your life that count. It's the life in your years.",
@@ -47,11 +48,11 @@ let quotes = [
 function getRandomQuote() {
 
     //generates a random number between 1 and length of quotes array; 
-    var randNum = Math.ceil(Math.random() * quotes.length - 1); 
+    let randNum = Math.ceil(Math.random() * quotes.length - 1); 
     //console.log(randNum);
 
     //fill a variable with random number
-    var quoteObj = quotes[randNum]; 
+    let quoteObj = quotes[randNum]; 
     //console.log(quoteObj);
 
     // return random number
@@ -63,15 +64,15 @@ function getRandomQuote() {
 ***/
 function printQuote(){
     //create variable with output from getRandomQuote function
-    var randObj = getRandomQuote(); 
+    let randObj = getRandomQuote(); 
     console.log(randObj);
     console.log(randObj.source, randObj.citation);
 
     //retrieve id of html element and uses it to fill variable 'quoteBox'
-    var quoteBox = document.querySelector('#quote-box'); 
+    const quoteBox = document.querySelector('#quote-box'); 
     
     //create variable 'randQuote' and fills with string to be used to display quotes
-    var randContent = "<p class=\"quote\">" + randObj.quote + "</p> <p class=\"source\">" + randObj.source; 
+    let randContent = "<p class=\"quote\">" + randObj.quote + "</p> <p class=\"source\">" + randObj.source; 
     
     //checks if extra properties exist in randObj, if so, they are added to a string & concatenated to main quote string
     if (randObj.citation){
@@ -80,26 +81,36 @@ function printQuote(){
     if (randObj.year){
       randContent += "<span class=\"year\"/>" + randObj.year + "</span>";
     }
-    if (randObj.tag){
-      randContent += "<span class=\"tag\"/> Tags: " + randObj.tag + "</span>";
+    if (randObj.country){
+      randContent += "<span class=\"country\"/> Country: " + randObj.country + "</span>";
     }
     //insert complete quote string into the quote HTML
     quoteBox.innerHTML = randContent + "</p>";
     console.log(randContent);
+    changeColor();
 }
-//set printQuote function to run every second
-var autoRun
-function autoFunc() {
-    autoRun = setInterval(printQuote, 1000);
+//set printQuote & autoChange functions to run every 7 seconds
+var autoRun;
+var autoChange;
+function autoPlay() {
+    autoRun = setInterval(printQuote, 7000);
+    autoChange = setInterval(changeColor, 7000);
 }
-//stop printQuote function running automatically
-function stopAuto() {
-    clearInterval(autoRun);
+//generate and return a random number between 1 & 255
+function randColor() {
+  return Math.floor(Math.random() * 256);
 }
-//buttons to start/stop auto run function
-document.getElementById('start').addEventListener("click", autoFunc, false);
-document.getElementById('stop').addEventListener("click", stopAuto, false);
+//assign randum numbers to variables to be used for r, g, b values. Then change body background to concatenated RGB values
+function changeColor(){
+let r = randColor();
+let g = randColor();
+let b = randColor();
+let bgColor = 'rgb(' + r + "," + g + "," + b + ')';
+//console.log(bgColor);
+document.body.style.backgroundColor = bgColor;
+}
 
+autoPlay();
 /***
  * click event listener for the print quote button
  * DO NOT CHANGE THE CODE BELOW!!
